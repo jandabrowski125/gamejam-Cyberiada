8b5fa38ab34c41ceaf06b2f436a39871c87816e6
Toma400 <thomasso_calgori@o2.pl>
2026-05-04 22:48:33 +0200

    Working Dialogue.json parser

--- My project/MMiH_NB.nim
+++ My project/MMiH_NB.nim
@@ -1,15 +1,33 @@
 import MMiH_NarrativeBuilder
+import std/tables
 import uing
 
 let app = initApp()
 
 proc placeholderMenuProc(_: MenuItem, _: Window) = discard
+proc exportAppByMenu(_: MenuItem, _: Window) =
+    discard exportApp(app)
+# proc dictMenuShow(_: MenuItem, _: Window) =
+#     # TODO: closing dictWindow closes also initial window
+#     let dictWindow = newWindow("Match Made In Heaven: Dictionary", 300, 300)
+#     show dictWindow
+
+# ODDZIELNE WINDOWSY
+# główny - dialogue nodes i branche zwizualizowane
+#   - dialogue edit (gdy klikniesz na node'a i dasz edit/dasz "nowy node" przy głównym)
+#   - dictionary
 
 proc main = 
     block MenuSetup:
+        let fileMenu    = newMenu("Files")
         let speakerMenu = newMenu("Manage speakers")
-        speakerMenu.addItem("Add speaker",    placeholderMenuProc)
-        speakerMenu.addItem("Remove speaker", placeholderMenuProc)
+        # let dictMenu    = newMenu("Manage dictionary")
+        addItem(fileMenu,    "Export all files", exportAppByMenu)
+        addItem(speakerMenu, "Add speaker [unavailable]",      placeholderMenuProc)
+        addItem(speakerMenu, "Remove speaker [unavailable]",   placeholderMenuProc)
+        # addItem(dictMenu,    "Show dictionary",  dictMenuShow)
+        # addItem(dictMenu,    "Add word",         placeholderMenuProc)
+        # addItem(dictMenu,    "Remove word",      placeholderMenuProc)
     let speakerGroup  = newGroup("Speakers", true)
     let speakerSelect = newRadioButtons()
     for speakerName in getSpeakers(app):
@@ -18,11 +36,11 @@ proc main =
     let window   = newWindow("Match Made In Heaven: Narrative Editor", 500, 500, hasMenubar=true)
     window.child       = speakerGroup
     speakerGroup.child = speakerSelect
+
+    echo app.dlist["28ec963c-3b86-4b90-9b42-c79f4572aea7"]
   
     show window
     mainLoop()
 
 init()
 main()
-if true: # block by something later
-  discard exportApp(app)
--- My project/MMiH_NarrativeBuilder.nim
+++ My project/MMiH_NarrativeBuilder.nim
@@ -12,6 +12,7 @@ type
     name* : string
   Choice = object
     msg      : string
+    speaker  : Speaker
     followup : string
     paragon  : int
     reneg    : int
@@ -21,10 +22,35 @@ type
     next    : string # ID from `dlist` to be forwarded later
     speaker : Speaker
     choices : seq[Choice]
+    # # non-exportable data
+    # sentlst : seq[string] # `msg` split by dots (for selection of sentences)
+    # sentctx : int         # index of selected sentence?
 
 const SPKR_PATH = "Assets/Characters.json"
 const DICT_PATH = "Assets/KnownWords.json"
-const DIAL_PATH = "Assets/App/Dialogue.json"
+const DIAL_PATH = "Assets/Dialogues/Dialogue.json"
+
+# TEMPORARY
+# meant to be later built into simpler seq[string] importer (validator might remain)
+proc dictionaryValidator (d: var Dictionary) =
+    # ensures consistent letters and sorting
+    const REPLACEMENTS = {
+        "’": "'"
+    }.toTable
+    for ix, w in d.words.pairs():
+      for RE in REPLACEMENTS.keys():
+        if RE in w:
+          d.words[ix] = replace(w, RE, REPLACEMENTS[RE])
+    sort(d.words)
+
+proc dictionaryImport (): Dictionary =
+    let df = open(DICT_PATH)
+    let dj = parseJson(df.readAll())
+    for word_node in dj.items():
+      add(result.words, word_node.getStr())
+    dictionaryValidator(result)
+    close(df)
+# TEMPORARY END
 
 proc speakersGet (): seq[Speaker] =
     let df = open(SPKR_PATH)
@@ -33,14 +59,42 @@ proc speakersGet (): seq[Speaker] =
       let actobj = Speaker(name: act.getStr())
       add(result, actobj)
     close(df)
+
+proc dialogueGet (speakerList: seq[Speaker], dialList: JsonNode): OrderedTable[string, Dialogue] =
+    # : dialList - should be result of parsing Dialogue.json and getting its ["nodes"] key (which is meant to be JArray type)
+    # echo dialList.kind
+    for dialNode in dialList.items():
+        var dial = Dialogue(msg     : getOrDefault(dialNode, "text_original").getStr("[ERROR]"),
+                            wordle  : getOrDefault(dialNode, "wordle_solution").getStr(""),
+                            next    : getOrDefault(dialNode, "next_node").getStr("[ERROR]"), # "" (last node symbol) should still load to not reach default
+                            speaker : Speaker(name: getOrDefault(dialNode, "speaker").getStr("[ERROR]")),
+                            choices : newSeq[Choice]())
+        for chcNode in getOrDefault(dialNode, "choices").items():
+            dial.choices.add(Choice(msg      : getOrDefault(chcNode, "text").getStr("[ERROR]"),
+                                    followup : getOrDefault(chcNode, "follow_up").getStr("[ERROR]"),
+                                    paragon  : getOrDefault(chcNode, "plus_paragon").getInt(0),
+                                    reneg    : getOrDefault(chcNode, "plus_renegade").getInt(0)))
+        # if dial.msg == "": return ERROR_ID
+        # for dial.choices: if chc.msg/followup == "": return ERROR_ID
+        # if dial.speaker notin speakerList == "": return ERROR_ID
+        # Error object? (aka types: "too long choice seq" MinorError, "empty string" EmptyError, "not parsed correctly" MajorError)
+        result[getOrDefault(dialNode, "node_id").getStr()] = dial
    
 # === MAIN GAME CONSTRUCTOR === 
 type
   App = object
     speakers : seq[Speaker]
-    dlist    : Table[string, Dialogue]
+    wdict    : seq[string]
+    dlist*   : OrderedTable[string, Dialogue]
+    # errs     : seq[string] # IDs of dialogues that contain [ERROR] or [EMPTY]
+    
 proc initApp* (): App =
     result.speakers = speakersGet()
+    result.wdict    = dictionaryImport().words
+    block dialogues: # unblock it later and make it less verbose on init (migrate all stuff into `dialogueGet`)
+        let df = open(DIAL_PATH)
+        let dj = parseJson(df.readAll())
+        result.dlist = dialogueGet(result.speakers, dj["nodes"])
     
 proc getSpeakers* (a: App): seq[string] =
     # used so that you don't access object; both for GUI usage and JSON export
@@ -53,6 +107,9 @@ proc addSpeaker* (a: var App, name: string): bool =
       if speaker.name == name: return false
     add(a.speakers, Speaker(name: name))
     return true
+
+proc getDictionary* (a: App): seq[string] =
+    return a.wdict
     
 proc exportApp* (a: App): bool =
     # returns if succeed to proceed with all exports
@@ -93,25 +150,6 @@ proc dictionaryRemove (d: var Dictionary, s: string) =
     let ix = find(d.words, s)
     if ix != -1: # if exists
       delete(d.words, ix)
-
-proc dictionaryValidator (d: var Dictionary) =
-    # ensures consistent letters and sorting
-    const REPLACEMENTS = {
-        "’": "'"
-    }.toTable
-    for ix, w in d.words.pairs():
-      for RE in REPLACEMENTS.keys():
-        if RE in w:
-          d.words[ix] = replace(w, RE, REPLACEMENTS[RE])
-    sort(d.words)
-
-proc dictionaryImport (): Dictionary =
-    let df = open(DICT_PATH)
-    let dj = parseJson(df.readAll())
-    for word_node in dj.items():
-      add(result.words, word_node.getStr())
-    dictionaryValidator(result)
-    close(df)
 
 proc dictionaryExport (d: Dictionary) =
     let df = open(DICT_PATH, fmWrite)
